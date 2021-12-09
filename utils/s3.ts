1a9fbd2cba0d29e51e26244d68d169104afa0335
Tomi Jaga <jagatomi12@gmail.com>
2021-12-09 17:07:10 -0500

    new env

--- utils/s3.ts
+++ utils/s3.ts
@@ -1,13 +1,13 @@
 import S3 from "aws-sdk/clients/s3";
 import { createReadStream } from "fs";
 
-const { AWS_ACCESS_KEY, AWS_SECRET_KEY, AWS_BUCKET_NAME, AWS_BUCKET_REGION } =
+const { AWS_ACCESS_KEY, AWS__SECRET_KEY, AWS_BUCKET_NAME, AWS_BUCKET_REGION } =
   process.env;
 
 const s3 = new S3({
   region: AWS_BUCKET_REGION,
   accessKeyId: AWS_ACCESS_KEY,
-  secretAccessKey: AWS_SECRET_KEY,
+  secretAccessKey: AWS__SECRET_KEY,
 });
 
 export const uploadImageToS3 = async ({
